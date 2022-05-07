0af03a2ad33a7fc281483356a0ce104b2644d05e
wutdubs <u201817841@upc.edu.pe>
2022-05-07 05:07:28 -0500

    feat: services history
    Added services history component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {HomeComponent} from "./public/home/home.component";
 import {HelpComponent} from "./public/help/help.component";
 import {CasesComponent} from "./public/cases/pages/cases.component";
 import {TechniciansComponent} from "./technicians/pages/technicians/technicians.component";
+import {ServiceshistoryComponent} from "./public/serviceshistory/serviceshistory.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'technicians', component: TechniciansComponent },
   { path: 'help', component: HelpComponent },
   { path: 'cases', component: CasesComponent },
+  { path: 'services-history', component: ServiceshistoryComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
