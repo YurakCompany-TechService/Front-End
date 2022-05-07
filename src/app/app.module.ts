5e9a5d194bc6519ad4c0177ad64015d51393e89f
a-202011779 <u202011779@upc.edu.pe>
2022-05-06 23:17:55 -0500

    feat: Added Help Section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import { HomeComponent } from './public/home/home.component';
+import { HelpComponent } from './public/help/help.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTableModule} from "@angular/material/table";
 import {MatDividerModule} from "@angular/material/divider";
+import {MatTabsModule} from "@angular/material/tabs";
+import {CdkAccordionModule} from "@angular/cdk/accordion";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +31,8 @@ import {MatDividerModule} from "@angular/material/divider";
     MatIconModule,
     MatTableModule,
     MatDividerModule,
+    MatTabsModule,
+    CdkAccordionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
