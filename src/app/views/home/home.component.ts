e2ceb741b93d024e7e3358d23f0f8b0360115b73
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2022-05-28 23:17:09 -0500

    fix: Fixed the introduction of the general home

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,11 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  img: string = 'img-business'
   ngOnInit(): void {
   }
-  images = [
-    {path: 'https://miro.medium.com/max/512/1*gtivoXf3wX0sDj16YQ1sXQ.jpeg'},
-    {path: 'https://blog.up.edu.mx/hubfs/Blog/2019/Enero/ingenieria-inteligencia-artificial-%20programcion-competitiva-up-ags.png'},
-  ]
+
 }
